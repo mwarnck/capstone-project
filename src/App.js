655ccfe8e1f12bb3246f0bbaa9967eb1554d841e
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-07 12:01:07 +0100

    Add page for drinklist and fill with fetched drinks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import { useState } from 'react';
-import Button from './Button.js';
-import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import DrinkList from './pages/DrinkList.js';
 
 function App() {
   const [drinks, setDrinks] = useState([]);
 
-  console.log(drinks);
+  useEffect(() => {
+    getDrinks();
+  }, []);
+
   return (
     <div>
-      <Button handleClick={getDrinks}>Show Drinks</Button>
-      {drinks &&
-        drinks.map(drink => (
-          <DrinkContainer key={drink.idDrink}>
-            <p>{drink.strDrink}</p>
-            <p>{drink.strIngredient1}</p>
-            <p>{drink.strIngredient2}</p>
-            <p>{drink.strIngredient3}</p>
-            <p>{drink.strIngredient4}</p>
-            <p>{drink.strIngredient5}</p>
-            <p>{drink.strIngredient6}</p>
-            <p>{drink.strIngredient7}</p>
-            <p>{drink.strIngredient8}</p>
-          </DrinkContainer>
-        ))}
+      <DrinkList drinks={drinks} />
     </div>
   );
 
@@ -38,7 +26,3 @@ function App() {
 }
 
 export default App;
-
-const DrinkContainer = styled.div`
-  border: 1px solid black;
-`;
