93b7b2a1c12dda146475e7adc08fca3b6b512d99
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-08 11:06:44 +0100

    Add image to DrinkCard and modify styling for wider screens

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -4,54 +4,62 @@ export default function DrinkCard({ drink }) {
   return (
     <DrinkContainer>
       <DrinkName>{drink.strDrink}</DrinkName>
-      <IngredientsList>
-        <li>
-          <strong>{drink.strIngredient1}</strong> {drink.strMeasure1}
-        </li>
-        <li>
-          <strong>{drink.strIngredient2}</strong> {drink.strMeasure2}
-        </li>
-        <li>
-          <strong>{drink.strIngredient3}</strong> {drink.strMeasure3}
-        </li>
-        <li>
-          <strong>{drink.strIngredient4}</strong> {drink.strMeasure4}
-        </li>
-        <li>
-          <strong>{drink.strIngredient5}</strong> {drink.strMeasure5}
-        </li>
-        <li>
-          <strong>{drink.strIngredient6}</strong> {drink.strMeasure6}
-        </li>
-        <li>
-          <strong>{drink.strIngredient7}</strong> {drink.strMeasure7}
-        </li>
-        <li>
-          <strong>{drink.strIngredient8}</strong> {drink.strMeasure8}
-        </li>
-        <li>
-          <strong>{drink.strIngredient9}</strong> {drink.strMeasure9}
-        </li>
-        <li>
-          <strong>{drink.strIngredient10}</strong> {drink.strMeasure10}
-        </li>
-        <li>
-          <strong>{drink.strIngredient11}</strong> {drink.strMeasure11}
-        </li>
-        <li>
-          <strong>{drink.strIngredient12}</strong> {drink.strMeasure12}
-        </li>
-        <li>
-          <strong>{drink.strIngredient13}</strong> {drink.strMeasure13}
-        </li>
-        <li>
-          <strong>{drink.strIngredient14}</strong> {drink.strMeasure14}
-        </li>
-        <li>
-          <strong>{drink.strIngredient15}</strong> {drink.strMeasure15}
-        </li>
-      </IngredientsList>
-      <DrinkInstruction>{drink.strInstructions}</DrinkInstruction>
+      <Wrapper>
+        <DrinkImage
+          src={drink.strDrinkThumb}
+          alt={drink.strDrink}
+          width={200}
+          height={200}
+        ></DrinkImage>
+        <IngredientsList role="list">
+          <li>
+            <strong>{drink.strIngredient1}</strong> {drink.strMeasure1}
+          </li>
+          <li>
+            <strong>{drink.strIngredient2}</strong> {drink.strMeasure2}
+          </li>
+          <li>
+            <strong>{drink.strIngredient3}</strong> {drink.strMeasure3}
+          </li>
+          <li>
+            <strong>{drink.strIngredient4}</strong> {drink.strMeasure4}
+          </li>
+          <li>
+            <strong>{drink.strIngredient5}</strong> {drink.strMeasure5}
+          </li>
+          <li>
+            <strong>{drink.strIngredient6}</strong> {drink.strMeasure6}
+          </li>
+          <li>
+            <strong>{drink.strIngredient7}</strong> {drink.strMeasure7}
+          </li>
+          <li>
+            <strong>{drink.strIngredient8}</strong> {drink.strMeasure8}
+          </li>
+          <li>
+            <strong>{drink.strIngredient9}</strong> {drink.strMeasure9}
+          </li>
+          <li>
+            <strong>{drink.strIngredient10}</strong> {drink.strMeasure10}
+          </li>
+          <li>
+            <strong>{drink.strIngredient11}</strong> {drink.strMeasure11}
+          </li>
+          <li>
+            <strong>{drink.strIngredient12}</strong> {drink.strMeasure12}
+          </li>
+          <li>
+            <strong>{drink.strIngredient13}</strong> {drink.strMeasure13}
+          </li>
+          <li>
+            <strong>{drink.strIngredient14}</strong> {drink.strMeasure14}
+          </li>
+          <li>
+            <strong>{drink.strIngredient15}</strong> {drink.strMeasure15}
+          </li>
+        </IngredientsList>
+        <DrinkInstruction>{drink.strInstructions}</DrinkInstruction>
+      </Wrapper>
     </DrinkContainer>
   );
 }
@@ -63,18 +71,34 @@ const DrinkContainer = styled.div`
 `;
 
 const DrinkName = styled.h2`
-  margin-left: 20px;
+  padding-left: 20px;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   color: #f28d35;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+const DrinkImage = styled.img`
+  border-radius: 5px;
+  margin-bottom: 20px;
 `;
 
 const IngredientsList = styled.ul`
   list-style: none;
   font-size: 0.8rem;
+  padding-right: 20px;
+  padding-left: 20px;
 `;
 
 const DrinkInstruction = styled.p`
-  margin-left: 20px;
-  margin-right: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+  max-width: 600px;
   font-size: 0.9rem;
 `;
