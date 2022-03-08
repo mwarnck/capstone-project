3b3b398aee68dde86e4d82a72f011f91bc96d789
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-08 13:17:35 +0100

    Modify test for rendering the component content

--- src/components/DrinkCard.spec.js
+++ src/components/DrinkCard.spec.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import DrinkCard from './DrinkCard';
 
 describe('DrinkCard', () => {
-  it('render div with name, picture, ingredients and instructions for a drink', () => {
+  it('render div with name, picture, button, ingredients and instructions for a drink', () => {
     render(
       <DrinkCard
         drink={{
@@ -28,10 +29,51 @@ describe('DrinkCard', () => {
     const image = screen.getByRole('img');
     expect(image).toBeInTheDocument();
 
+    const button = screen.getByRole('button', { name: /Show/i });
+    expect(button).toBeInTheDocument();
+
+    userEvent.click(button);
+
     const ingredients = screen.getByRole('list');
     expect(ingredients).toBeInTheDocument();
 
     const instructions = screen.getByText(/Muddle mint/i);
     expect(instructions).toBeInTheDocument();
+
+    userEvent.click(button);
+
+    expect(ingredients).not.toBeInTheDocument();
+    expect(instructions).not.toBeInTheDocument();
+  });
+
+  it('clicking on the button calls the function toggleDetails', () => {
+    const handleClick = jest.fn();
+    render(
+      <DrinkCard
+        toggleDetails={handleClick}
+        detailsVisable={false}
+        drink={{
+          strDrink: 'Mojito',
+          strDrinkThumb:
+            'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+          strIngredient1: 'lime',
+          strMeasure1: 'juice of 1',
+          strInstructions: `Muddle mint leaves with sugar 
+    and lime juice. Add a splash 
+    of soda water and fill the 
+    glass with cracked ice. Pour 
+    the rum and top with soda 
+    water. Garnish and serve with 
+    straw`,
+        }}
+      />
+    );
+
+    const button = screen.getByRole('button', { name: /show/i });
+    expect(button).toBeInTheDocument();
+
+    userEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalled();
   });
 });
