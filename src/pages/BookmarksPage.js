064b05f14ae3e200a8c3628c9e8dae64d20f6a53
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-09 14:14:12 +0100

    Add button for marking as bookmarked to drink card

--- src/pages/BookmarksPage.js
+++ src/pages/BookmarksPage.js
@@ -13,7 +13,8 @@ export default function DrinkList({ drinks }) {
 const DrinklistContainer = styled.section`
   display: grid;
   gap: 10px;
-
+  padding-left: 10px;
+  padding-right: 10px;
   margin-bottom: 0;
   margin-top: 0;
 `;
