f3afda9f7cea71a54011c8852754e7ebdfa7207c
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-11 12:45:47 +0100

    Add test for searchbar

--- src/components/Searchbar.spec.js
+++ src/components/Searchbar.spec.js
@@ -1,10 +1,25 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import Searchbar from './Searchbar'
-  
-  describe('Searchbar', () => {
-    it('' , () => {
-      render(<Searchbar />)
-    })
-  })
-  
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Searchbar from './Searchbar';
+
+describe('Searchbar', () => {
+  it('render a label and a input element', () => {
+    render(<Searchbar />);
+
+    const inputElement = screen.getByLabelText(/Search/i);
+
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toHaveAttribute('placeholder', 'e.g. Caipirinha...');
+  });
+
+  it('calls onCange function when change input value', () => {
+    const changeValue = jest.fn();
+    render(<Searchbar handleChange={changeValue} />);
+
+    const inputElement = screen.getByLabelText(/Search/i);
+
+    userEvent.type(inputElement, 'A');
+
+    expect(changeValue).toHaveBeenCalled();
+  });
+});
