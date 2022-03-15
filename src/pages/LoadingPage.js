6179ca2dd0cfa09bcfeb724eea3aa1186c433075
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-15 11:51:55 +0100

    Add and import image for loading screen, add styles

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,19 +1,50 @@
 import styled from 'styled-components';
+import loadingImage from '../images/loadingPageImage.jpg';
 
 export default function LoadingPage() {
   return (
     <LoadingScreen>
-      <img
-        src="https://source.unsplash.com/random/400×900/?cocktail"
-        alt="loading screen"
+      <IntroTitel>Cocktail Night</IntroTitel>
+      <IntroImage
+        src={loadingImage}
+        alt="Orange Cocktail with dark background"
         width="400"
         height="900"
-      />
+      ></IntroImage>
     </LoadingScreen>
   );
 }
 
 const LoadingScreen = styled.div`
-  margin: 0 auto;
   position: relative;
+  width: 100%;
+  height: 100vh;
+  background: #000;
+`;
+
+const IntroTitel = styled.p`
+  position: absolute;
+  z-index: 1;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  text-align: center;
+  top: 60px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 3rem;
+  font-weight: bold;
+  color: #f28d35;
+`;
+
+const IntroImage = styled.img`
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
+  text-align: center;
+  min-width: 400px;
+  max-width: 800px;
+  height: 100%;
 `;
