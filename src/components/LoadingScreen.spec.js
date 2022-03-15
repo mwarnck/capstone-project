a60bd60880c133797eed6914bedb889556a081b4
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-15 12:03:38 +0100

    Create test for rendering image and titel of loading screen

--- src/components/LoadingScreen.spec.js
+++ src/components/LoadingScreen.spec.js
@@ -1,10 +1,14 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import LoadingScreen from './LoadingScreen'
-  
-  describe('LoadingScreen', () => {
-    it('' , () => {
-      render(<LoadingScreen />)
-    })
-  })
-  
+import { render, screen } from '@testing-library/react';
+import LoadingScreen from './LoadingScreen';
+
+describe('LoadingScreen', () => {
+  it('renders an image and p element', () => {
+    render(<LoadingScreen />);
+
+    const loadingImage = screen.getByRole('img');
+    expect(loadingImage).toBeInTheDocument();
+
+    const loadingTitle = screen.getByText('Cocktail Night');
+    expect(loadingTitle).toBeInTheDocument();
+  });
+});
