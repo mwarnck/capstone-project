111e79dd76073385632220d7bffbf32a38e460eb
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-16 11:41:00 +0100

    Add further styling for single DrinkPage. Replace svg code for bookmark icons with img elements with the svg as src

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -1,5 +1,7 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import bookmarkActive from '../icons/sunBookmarkActive.svg';
+import bookmarkInactive from '../icons/sunBookmarkInactive.svg';
 
 export default function DrinkCard({ drink, toggleBookmark }) {
   return (
@@ -9,42 +11,19 @@ export default function DrinkCard({ drink, toggleBookmark }) {
         onClick={() => toggleBookmark(drink.idDrink)}
       >
         {drink.isBookmarked ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="img"
-            alt="isBookmarked"
+          <img
+            src={bookmarkActive}
             width="25"
             height="25"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 64 64"
-          >
-            <path
-              fill="#ffe62e"
-              d="m20.5 59.7l7-7.2c-2.5-.5-4.8-1.5-6.9-2.9l-.1 10.1m23-55.4l-7 7.2c2.5.5 4.8 1.5 6.9 2.9l.1-10.1M4.3 43.5l10.1-.1C13 41.3 12 39 11.5 36.5l-7.2 7m55.4-23l-10.1.1c1.3 2.1 2.3 4.4 2.9 6.9l7.2-7m-55.4 0l7.2 7c.5-2.5 1.5-4.8 2.9-6.9l-10.1-.1m55.4 23l-7.2-7c-.5 2.5-1.5 4.8-2.9 6.9l10.1.1M20.5 4.3l.1 10.1c2.1-1.3 4.4-2.3 6.9-2.9l-7-7.2m23 55.4l-.1-10.1C41.3 51 39 52 36.5 52.5l7 7.2"
-            />
-            <g fill="#ffce31">
-              <path d="m14.8 44l-4 9.3l9.3-4C18 47.8 16.2 46 14.8 44m34.4-24l4-9.3l-9.2 4c2 1.5 3.8 3.3 5.2 5.3m-37.8 8.3L2 32l9.4 3.7c-.3-1.2-.4-2.4-.4-3.7s.1-2.5.4-3.7m41.2 7.4L62 32l-9.4-3.7c.2 1.2.4 2.5.4 3.7c0 1.3-.1 2.5-.4 3.7M20 14.8l-9.3-4l4 9.3c1.5-2.1 3.3-3.9 5.3-5.3m24 34.4l9.3 4l-4-9.3C47.8 46 46 47.8 44 49.2m-8.3-37.8L32 2l-3.7 9.4c1.2-.2 2.5-.4 3.7-.4s2.5.1 3.7.4m-7.4 41.2L32 62l3.7-9.4c-1.2.3-2.4.4-3.7.4s-2.5-.1-3.7-.4" />
-              <circle cx="32" cy="32" r="19" />
-            </g>
-          </svg>
+            alt="active bookmark icon"
+          />
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="img"
-            alt="notBookmarked"
+          <img
+            src={bookmarkInactive}
             width="25"
             height="25"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 64 64"
-          >
-            <path
-              fill="currentColor"
-              d="m20.52 59.717l7.027-7.2a20.87 20.87 0 0 1-6.904-2.87l-.123 10.07M43.48 4.284l-7.025 7.199a20.872 20.872 0 0 1 6.904 2.871l.121-10.07m-31.996 32.17l-7.201 7.025l10.07-.122a20.85 20.85 0 0 1-2.869-6.903m41.032-8.907l7.201-7.027l-10.07.123a20.856 20.856 0 0 1 2.869 6.904m-38.162-6.905l-10.07-.123l7.201 7.027a20.805 20.805 0 0 1 2.869-6.904m35.292 22.716l10.07.122l-7.201-7.026a20.814 20.814 0 0 1-2.869 6.904M27.547 11.483l-7.027-7.2l.123 10.07a20.87 20.87 0 0 1 6.904-2.87m8.906 41.034l7.027 7.199l-.123-10.069a20.891 20.891 0 0 1-6.904 2.87m-21.701-8.555l-3.967 9.251l9.252-3.965a21.104 21.104 0 0 1-5.285-5.286m34.496-23.923l3.965-9.252l-9.25 3.965a21.108 21.108 0 0 1 5.285 5.287M11 32c0-1.278.133-2.524.352-3.741L2 31.999l9.352 3.74A21.034 21.034 0 0 1 11 32zm51 0l-9.352-3.741C52.867 29.476 53 30.722 53 32c0 1.279-.133 2.525-.352 3.741L62 32zM20.039 14.751l-9.252-3.965l3.965 9.252a21.149 21.149 0 0 1 5.287-5.287m23.922 34.497l9.252 3.965l-3.965-9.251a21.128 21.128 0 0 1-5.287 5.286M35.74 11.352L32 2l-3.74 9.352C29.475 11.133 30.721 11 32 11s2.525.133 3.74.352m-7.48 41.296L32 62l3.74-9.352c-1.215.219-2.461.352-3.74.352s-2.525-.133-3.74-.352"
-            />
-            <circle cx="32" cy="32" r="19" fill="currentColor" />
-          </svg>
+            alt="inactive bookmark icon"
+          />
         )}
         <span className="sr-only">Bookmark</span>
       </BookmarkButton>
--- src/components/DrinkPage.js
+++ src/components/DrinkPage.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import bookmarkActive from '../icons/sunBookmarkActive.svg';
+import bookmarkInactive from '../icons/sunBookmarkInactive.svg';
 
 export default function DrinkPage({ drink, toggleBookmark }) {
   return (
@@ -9,99 +11,132 @@ export default function DrinkPage({ drink, toggleBookmark }) {
         onClick={() => toggleBookmark(drink.idDrink)}
       >
         {drink.isBookmarked ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="img"
-            alt="isBookmarked"
+          <img
+            src={bookmarkActive}
             width="25"
             height="25"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 64 64"
-          >
-            <path
-              fill="#ffe62e"
-              d="m20.5 59.7l7-7.2c-2.5-.5-4.8-1.5-6.9-2.9l-.1 10.1m23-55.4l-7 7.2c2.5.5 4.8 1.5 6.9 2.9l.1-10.1M4.3 43.5l10.1-.1C13 41.3 12 39 11.5 36.5l-7.2 7m55.4-23l-10.1.1c1.3 2.1 2.3 4.4 2.9 6.9l7.2-7m-55.4 0l7.2 7c.5-2.5 1.5-4.8 2.9-6.9l-10.1-.1m55.4 23l-7.2-7c-.5 2.5-1.5 4.8-2.9 6.9l10.1.1M20.5 4.3l.1 10.1c2.1-1.3 4.4-2.3 6.9-2.9l-7-7.2m23 55.4l-.1-10.1C41.3 51 39 52 36.5 52.5l7 7.2"
-            />
-            <g fill="#ffce31">
-              <path d="m14.8 44l-4 9.3l9.3-4C18 47.8 16.2 46 14.8 44m34.4-24l4-9.3l-9.2 4c2 1.5 3.8 3.3 5.2 5.3m-37.8 8.3L2 32l9.4 3.7c-.3-1.2-.4-2.4-.4-3.7s.1-2.5.4-3.7m41.2 7.4L62 32l-9.4-3.7c.2 1.2.4 2.5.4 3.7c0 1.3-.1 2.5-.4 3.7M20 14.8l-9.3-4l4 9.3c1.5-2.1 3.3-3.9 5.3-5.3m24 34.4l9.3 4l-4-9.3C47.8 46 46 47.8 44 49.2m-8.3-37.8L32 2l-3.7 9.4c1.2-.2 2.5-.4 3.7-.4s2.5.1 3.7.4m-7.4 41.2L32 62l3.7-9.4c-1.2.3-2.4.4-3.7.4s-2.5-.1-3.7-.4" />
-              <circle cx="32" cy="32" r="19" />
-            </g>
-          </svg>
+            alt="active bookmark icon"
+          />
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            role="img"
-            alt="notBookmarked"
+          <img
+            src={bookmarkInactive}
             width="25"
             height="25"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 64 64"
-          >
-            <path
-              fill="currentColor"
-              d="m20.52 59.717l7.027-7.2a20.87 20.87 0 0 1-6.904-2.87l-.123 10.07M43.48 4.284l-7.025 7.199a20.872 20.872 0 0 1 6.904 2.871l.121-10.07m-31.996 32.17l-7.201 7.025l10.07-.122a20.85 20.85 0 0 1-2.869-6.903m41.032-8.907l7.201-7.027l-10.07.123a20.856 20.856 0 0 1 2.869 6.904m-38.162-6.905l-10.07-.123l7.201 7.027a20.805 20.805 0 0 1 2.869-6.904m35.292 22.716l10.07.122l-7.201-7.026a20.814 20.814 0 0 1-2.869 6.904M27.547 11.483l-7.027-7.2l.123 10.07a20.87 20.87 0 0 1 6.904-2.87m8.906 41.034l7.027 7.199l-.123-10.069a20.891 20.891 0 0 1-6.904 2.87m-21.701-8.555l-3.967 9.251l9.252-3.965a21.104 21.104 0 0 1-5.285-5.286m34.496-23.923l3.965-9.252l-9.25 3.965a21.108 21.108 0 0 1 5.285 5.287M11 32c0-1.278.133-2.524.352-3.741L2 31.999l9.352 3.74A21.034 21.034 0 0 1 11 32zm51 0l-9.352-3.741C52.867 29.476 53 30.722 53 32c0 1.279-.133 2.525-.352 3.741L62 32zM20.039 14.751l-9.252-3.965l3.965 9.252a21.149 21.149 0 0 1 5.287-5.287m23.922 34.497l9.252 3.965l-3.965-9.251a21.128 21.128 0 0 1-5.287 5.286M35.74 11.352L32 2l-3.74 9.352C29.475 11.133 30.721 11 32 11s2.525.133 3.74.352m-7.48 41.296L32 62l3.74-9.352c-1.215.219-2.461.352-3.74.352s-2.525-.133-3.74-.352"
-            />
-            <circle cx="32" cy="32" r="19" fill="currentColor" />
-          </svg>
+            alt="inactive bookmark icon"
+          />
         )}
         <span className="sr-only">Bookmark</span>
       </BookmarkButton>
+      <ShortFactsList role="list">
+        <li>{drink.strCategory}</li>
+        <li>{drink.strAlcoholic}</li>
+        <li>{drink.strGlass}</li>
+      </ShortFactsList>
       <DrinkImage
         src={drink.strDrinkThumb}
         alt={drink.strDrink}
         width={250}
         height={250}
       ></DrinkImage>
-      <IngredientsList role="list">
-        <li>
-          <strong>{drink.strIngredient1}</strong> {drink.strMeasure1}
-        </li>
-        <li>
-          <strong>{drink.strIngredient2}</strong> {drink.strMeasure2}
-        </li>
-        <li>
-          <strong>{drink.strIngredient3}</strong> {drink.strMeasure3}
-        </li>
-        <li>
-          <strong>{drink.strIngredient4}</strong> {drink.strMeasure4}
-        </li>
-        <li>
-          <strong>{drink.strIngredient5}</strong> {drink.strMeasure5}
-        </li>
-        <li>
-          <strong>{drink.strIngredient6}</strong> {drink.strMeasure6}
-        </li>
-        <li>
-          <strong>{drink.strIngredient7}</strong> {drink.strMeasure7}
-        </li>
-        <li>
-          <strong>{drink.strIngredient8}</strong> {drink.strMeasure8}
-        </li>
-        <li>
-          <strong>{drink.strIngredient9}</strong> {drink.strMeasure9}
-        </li>
-        <li>
-          <strong>{drink.strIngredient10}</strong> {drink.strMeasure10}
-        </li>
-        <li>
-          <strong>{drink.strIngredient11}</strong> {drink.strMeasure11}
-        </li>
-        <li>
-          <strong>{drink.strIngredient12}</strong> {drink.strMeasure12}
-        </li>
-        <li>
-          <strong>{drink.strIngredient13}</strong> {drink.strMeasure13}
-        </li>
-        <li>
-          <strong>{drink.strIngredient14}</strong> {drink.strMeasure14}
-        </li>
-        <li>
-          <strong>{drink.strIngredient15}</strong> {drink.strMeasure15}
-        </li>
-      </IngredientsList>
-      <DrinkInstruction>{drink.strInstructions}</DrinkInstruction>
+      <IngredientsContainer>
+        <SubHeading>Ingredients:</SubHeading>
+        <IngredientsList role="list">
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient1}</strong>
+            </dt>
+            <dd>{drink.strMeasure1}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient2}</strong>
+            </dt>
+            <dd>{drink.strMeasure2}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient3}</strong>
+            </dt>
+            <dd>{drink.strMeasure3}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient4}</strong>
+            </dt>
+            <dd>{drink.strMeasure4}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient5}</strong>
+            </dt>
+            <dd>{drink.strMeasure5}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient6}</strong>
+            </dt>
+            <dd>{drink.strMeasure6}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient7}</strong>
+            </dt>
+            <dd>{drink.strMeasure7}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient8}</strong>
+            </dt>
+            <dd>{drink.strMeasure8}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient9}</strong>
+            </dt>
+            <dd>{drink.strMeasure9}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient10}</strong>
+            </dt>
+            <dd>{drink.strMeasure10}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient11}</strong>
+            </dt>
+            <dd>{drink.strMeasure11}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient12}</strong>
+            </dt>
+            <dd>{drink.strMeasure12}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient13}</strong>
+            </dt>
+            <dd>{drink.strMeasure13}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient14}</strong>
+            </dt>
+            <dd>{drink.strMeasure14}</dd>
+          </IngredientMeasureWrapper>
+          <IngredientMeasureWrapper>
+            <dt>
+              <strong>{drink.strIngredient15}</strong>
+            </dt>
+            <dd>{drink.strMeasure15}</dd>
+          </IngredientMeasureWrapper>
+        </IngredientsList>
+      </IngredientsContainer>
+      <InstructionsContainer>
+        <SubHeading>Instructions:</SubHeading>
+        <DrinkInstruction>{drink.strInstructions}</DrinkInstruction>
+      </InstructionsContainer>
     </DrinkContainer>
   );
 }
@@ -110,7 +145,6 @@ const DrinkContainer = styled.div`
   display: grid;
   gap: 25px;
   place-items: center;
-  border: 1px solid black;
   padding-top: 20px;
   position: relative;
 `;
@@ -128,17 +162,50 @@ const DrinkName = styled.h2`
   color: #f28d35;
 `;
 
+const ShortFactsList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+
+  li {
+    font-size: 0.9rem;
+    border: 1px solid black;
+    border-radius: 20px;
+    padding: 3px;
+  }
+`;
+
 const DrinkImage = styled.img`
   border-radius: 5px;
 `;
 
-const IngredientsList = styled.ul`
-  list-style: none;
+const IngredientsContainer = styled.div`
+  display: grid;
+  gap: 10px;
+`;
+
+const IngredientMeasureWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 60px;
+`;
+
+const SubHeading = styled.p`
+  font-weight: bold;
+  color: #f28d35;
+`;
+
+const IngredientsList = styled.dl`
   font-size: 0.9rem;
 `;
 
+const InstructionsContainer = styled.div`
+  display: grid;
+  gap: 10px;
+  margin: 5px 10px 20px;
+`;
+
 const DrinkInstruction = styled.p`
-  padding: 0 20px 20px;
-  max-width: 600px;
   font-size: 0.9rem;
 `;
