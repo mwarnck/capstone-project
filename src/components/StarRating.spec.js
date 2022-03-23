44b6b91732a7631385ee390dafe3a7be3d13ef9c
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-23 16:04:14 +0100

    Finalize test for star rating

--- src/components/StarRating.spec.js
+++ src/components/StarRating.spec.js
@@ -1,10 +1,24 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import StarRating from './StarRating'
-  
-  describe('StarRating', () => {
-    it('' , () => {
-      render(<StarRating />)
-    })
-  })
-  
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import StarRating from './StarRating';
+
+describe('StarRating', () => {
+  it('renders a heading and 5 buttons for rating', () => {
+    render(<StarRating />);
+
+    const heading = screen.getByText(/rate/i);
+    expect(heading).toBeInTheDocument();
+
+    const ratingButtons = screen.getAllByRole('button');
+    expect(ratingButtons).toHaveLength(5);
+  });
+
+  it('calls a function to set rating when clicking a button', () => {
+    const ratingCallback = jest.fn();
+    render(<StarRating saveRatingToDrink={ratingCallback} />);
+
+    const ratingButtons = screen.getAllByRole('button');
+    userEvent.click(ratingButtons[0]);
+    expect(ratingCallback).toHaveBeenCalled();
+  });
+});
