a6ea03776f39dc291fcd36db01981a00986a3d04
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-23 15:51:07 +0100

    Add styling to rating container and implement into drink page

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -5,34 +5,57 @@ export default function StarRating({ rating, saveRatingToDrink, id }) {
   const [hover, setHover] = useState(0);
 
   return (
-    <div className="star-rating">
-      {[...Array(5)].map((star, index) => {
-        index += 1;
-        return (
-          <StarRatingButton
-            type="button"
-            key={index}
-            rating={rating}
-            hover={hover}
-            index={index}
-            //className={index <= (hover || rating) ? 'on' : 'off'}
-            onClick={() => saveRatingToDrink(index, id)}
-            onMouseEnter={() => setHover(index)}
-            onMouseLeave={() => setHover(rating)}
-          >
-            <span className="star">&#9733;</span>
-          </StarRatingButton>
-        );
-      })}
-    </div>
+    <RatingContainer>
+      <SubHeading>Rate this drink:</SubHeading>
+      <StarWrapper>
+        {[...Array(5)].map((star, index) => {
+          index += 1;
+          return (
+            <StarRatingButton
+              type="button"
+              key={index}
+              rating={rating}
+              hover={hover}
+              index={index}
+              onClick={() => saveRatingToDrink(index, id)}
+              onMouseEnter={() => setHover(index)}
+              onMouseLeave={() => setHover(rating)}
+            >
+              <span>&#9733;</span>
+            </StarRatingButton>
+          );
+        })}
+      </StarWrapper>
+    </RatingContainer>
   );
 }
 
+const RatingContainer = styled.div`
+  margin-top: 20px;
+  display: grid;
+  gap: 15px;
+  min-width: 220px;
+  max-width: 70%;
+`;
+
+const SubHeading = styled.p`
+  font-weight: bold;
+  color: var(--font-color-headlines-bright);
+`;
+
+const StarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const StarRatingButton = styled.button`
   background-color: transparent;
+  font-size: 1.5rem;
   border: none;
   outline: none;
   cursor: pointer;
   color: ${props =>
-    props.index <= (props.hover || props.rating) ? 'hotpink' : 'yellow'};
+    props.index <= (props.hover || props.rating)
+      ? 'var(--bg-color-bookmark)'
+      : 'var(--bg-color-secondary)'};
 `;
--- src/components/StarRating.stories.js
+++ src/components/StarRating.stories.js
@@ -1,11 +1,13 @@
-import StarRating from './StarRating'
-  
+import StarRating from './StarRating';
+
 export default {
   title: 'Component/StarRating',
   component: StarRating,
-}
+};
 
 const Template = args => <StarRating {...args} />;
 
-export const Default = Template.bind({})
-Default.args = {};
+export const Default = Template.bind({});
+Default.args = {
+  rating: '3',
+};
