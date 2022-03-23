4c361c55546782f25d3978befdc6bbb845f211ca
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-23 18:57:07 +0100

    Modify test for CommentBox

--- src/components/CommentBox.spec.js
+++ src/components/CommentBox.spec.js
@@ -1,10 +1,51 @@
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import CommentBox from './CommentBox'
-  
-  describe('CommentBox', () => {
-    it('' , () => {
-      render(<CommentBox />)
-    })
-  })
-  
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CommentBox from './CommentBox';
+
+describe('CommentBox', () => {
+  it('renders a heading, a textarea and a button', () => {
+    render(<CommentBox />);
+
+    const heading = screen.getByText(/leave/i);
+    expect(heading).toBeInTheDocument();
+
+    const textarea = screen.getByPlaceholderText('Add a comment...');
+    expect(textarea).toBeInTheDocument();
+
+    const submitButton = screen.getByRole('button');
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('when the drink has comments in its data, a li element should be rendered', () => {
+    render(
+      <CommentBox
+        comments={[{ commentId: '123', commentText: 'dies ist ein Comment' }]}
+      />
+    );
+
+    const commentItem = screen.getByRole('listitem');
+    expect(commentItem).toBeInTheDocument();
+  });
+
+  it('when clicking the button a submit function is called', () => {
+    const submitCallback = jest.fn();
+    const onChangeCallback = jest.fn();
+    render(
+      <CommentBox
+        handleSubmitComment={submitCallback}
+        setCommentValue={onChangeCallback}
+        commentValue={true}
+      />
+    );
+
+    const textarea = screen.getByPlaceholderText('Add a comment...');
+    userEvent.type(textarea, 'test');
+
+    expect(onChangeCallback).toHaveBeenCalled();
+
+    const submitButton = screen.getByRole('button');
+    userEvent.click(submitButton);
+
+    expect(submitCallback).toHaveBeenCalled();
+  });
+});
