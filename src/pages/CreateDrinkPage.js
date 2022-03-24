49aa4a893ce673becfddb1886bef97e0a435d853
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-24 14:39:57 +0100

    Add CreateDrinkPage and import form for creating new drinks

---
+++ src/pages/CreateDrinkPage.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import CreateDrinkForm from '../components/CreateDrinkForm.js';
+
+export default function CreateDrinkPage() {
+  return <CreateDrinkForm />;
+}
