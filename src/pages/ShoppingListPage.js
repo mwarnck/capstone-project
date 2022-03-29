6e27d8f39f5713e579e71a22227228e5935c3174
Mirco Warnck <mirco.warnck@googlemail.com>
2022-03-29 16:54:09 +0200

    Add logic to add selected drinks to an array and further logic to only generate list of ingredients to render

--- src/pages/ShoppingListPage.js
+++ src/pages/ShoppingListPage.js
@@ -7,9 +7,12 @@ import { useState } from 'react';
 export default function ShoppingListPage({ drinks }) {
   const [value, setValue] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [selectedDrinks, setSelectedDrinks] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
-  console.log(suggestions);
-  console.log(value);
+  //console.log(suggestions);
+  //console.log(value);
+  //console.log(selectedDrinks);
 
   const escapeRegexCharacters = str =>
     str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -34,6 +37,15 @@ export default function ShoppingListPage({ drinks }) {
         }}
         highlightFirstSuggestion={true}
       />
+
+      <ul>
+        Selected drinks:
+        {selectedDrinks.map(drink => (
+          <li key={drink.idDrink}>{drink.strDrink}</li>
+        ))}
+      </ul>
+      <button onClick={getIngredients}>Generate list of ingredients</button>
+      <ul>{ingredients && ingredients.map(ing => <li key={ing}>{ing}</li>)}</ul>
       <Navigation />
     </PageContainer>
   );
@@ -55,11 +67,37 @@ export default function ShoppingListPage({ drinks }) {
   }
 
   function renderSuggestion(suggestion) {
-    return <span>{suggestion.strDrink}</span>;
+    return (
+      <button onClick={() => handleSuggestionClick(suggestion)}>
+        {suggestion.strDrink}
+      </button>
+    );
+  }
+
+  function handleSuggestionClick(suggestion) {
+    setSelectedDrinks([...selectedDrinks, suggestion]);
+    setTimeout(() => {
+      setValue('');
+    }, 200);
+  }
+
+  function getIngredients() {
+    const filteredObjects = selectedDrinks.map(object =>
+      Object.fromEntries(
+        Object.entries(object).filter(([key]) => key.includes('Ingredient'))
+      )
+    );
+
+    const ingredientsArray = filteredObjects.map(obj => Object.values(obj));
+    const listOfIngredients = ingredientsArray
+      .flat()
+      .filter(element => element !== null);
+    const filteredIngredients = [...new Set(listOfIngredients)];
+    setIngredients(filteredIngredients);
   }
 }
 
 const PageContainer = styled.main`
   display: grid;
-  grid-template-rows: 48px 1fr;
+  grid-template-rows: 48px 1fr 1fr;
 `;
